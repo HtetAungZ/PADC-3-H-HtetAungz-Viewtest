b04dadf319a905a44005548c47324823b86d6811
DESKTOP-0DQPE5H\ICE <saberlinger98@gmail.com>
2017-12-10 12:02:27 +0630

    no message

---
+++ app/src/main/java/com/padcmyanmar/viewtesting/NewsDetailsActivity.java
@@ -0,0 +1,19 @@
+package com.padcmyanmar.viewtesting;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by ICE on 09-12-2017.
+ */
+
+public class NewsDetailsActivity extends AppCompatActivity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_news_details);
+    }
+
+}
---
+++ app/src/main/java/com/padcmyanmar/viewtesting/viewholders/ItemNewsViewHolder.java
@@ -0,0 +1,16 @@
+package com.padcmyanmar.viewtesting.viewholders;
+
+import android.support.v7.widget.RecyclerView;
+import android.view.View;
+
+/**
+ * Created by ICE on 03-12-2017.
+ */
+
+public class ItemNewsViewHolder extends RecyclerView.ViewHolder {
+
+
+    public ItemNewsViewHolder(View itemView) {
+        super(itemView);
+    }
+}
